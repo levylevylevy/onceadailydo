524771fdce6db96b53b17dd9e76a7117e79e1995
Sadie Levy <69177097+levylevylevy@users.noreply.github.com>
2020-09-12 18:14:29 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,65 @@
-import 'package:HopHacks/screens/AchievementListScreen.dart';
-import 'package:HopHacks/screens/ActivitySuggestionScreen.dart';
-import 'package:HopHacks/screens/NewUserFormScreen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-void main() {
-  runApp(OnceADailyDo());
-}
 
-/// The main class for this app.
-class OnceADailyDo extends StatelessWidget {
+void main() => runApp(StreaksScreen());
+
+class StreaksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/suggestions',
-      routes: {
-        '/': (context) => NewUserFormScreen(),
-        '/suggestions': (context) => ActivitySuggestionScreen(),
-        '/achievements': (context) => AchievementListScreen(),
-      }
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CalendarController _control;
+
+  @override
+  void initState() {
+    super.initState();
+    _control = CalendarController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Activity Streaks'),
+      ),
+
+
+
+
+
+      body:SingleChildScrollView(
+        child: new Align(
+          alignment: Alignment.bottomCenter,
+        child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+
+TableCalendar(
+  initialCalendarFormat: CalendarFormat.month,
+  calendarStyle: CalendarStyle(
+    todayColor: Color(0xFF566575),
+        selectedColor: Colors.orange,
+
+
+  ),
+
+  calendarController: _control,)
+      ],
+    ),
+      ),
+      )
     );
   }
 }
