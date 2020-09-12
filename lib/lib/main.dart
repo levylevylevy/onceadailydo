4cf23e09515032375f09cebc74d5d6b2ea3b91a5
Sadie Levy <69177097+levylevylevy@users.noreply.github.com>
2020-09-12 15:39:48 -0400

    Add files via upload

---
+++ lib/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Splashlogo(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class Splashlogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 7,
+      navigateAfterSeconds: new Home(),
+      backgroundColor: Color(0xFFF5F5F5),
+      image: new Image.network('images/oaddlogosplash-01.png'),
+      photoSize: 100.0,
+      loaderColor: Colors.orange,
+    );
+  }
+}
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF545F5),
+
+    );
+  }
+}
+
