3a05687ab19d1de0851bee5f52938e8e7caed453
Garrett Smith <garrett.smit7@gmail.com>
2020-09-13 02:17:04 -0400

    come up with better example

--- lib/models/MockActivityManager.dart
+++ lib/models/MockActivityManager.dart
@@ -11,8 +11,11 @@ class MockActivityManager {
   Future<void> loadActivitiesFromServer() async {
     this.activities.add(
           Activity.fromJson(
+            // would love to break this up into a multiline string, but when I do, I think it
+            // inserts newline characters into the string, so for now, it'll remain a long
+            // single-line string.
             json.decode(
-              '{"activityId": 1, "title": "Stack It Up", "text": "We think you\'d love the song Stack It Up by Liam Payne. Give it a listen at https://www.example.com/20FxLQ20X"}',
+              '{"activityId": 1, "title": "Taking Care of the Community","text": "Look up on google where your closest animal rescue is, and schedule to help volunteer for an hour. Trust us, you\'ll really enjoy this one! ![dog volunteer](https://www.littlewhitedogco.com/wp-content/uploads/2019/10/volunteer-1200x630.jpg)"}',
             ),
           ),
         );
