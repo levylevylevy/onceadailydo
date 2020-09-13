c0aa45aa8e9a85e68713e85937272c2a0ec581fc
Sadie Levy <69177097+levylevylevy@users.noreply.github.com>
2020-09-12 21:36:56 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,29 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return MainHeaderAndNavigation(
         title: 'Activity Streaks',
+
         child: Container(
             child: Center(
                 child: Column(
           children: <Widget>[
             Container(
+
               color: Color(0xFF797983),
               height: 184,
               width: 600,
+              child: new Text(
+                'current streak:',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Color(0xFFF5F5F5),
+                ),
+
+              ),
+              alignment: AlignmentDirectional(0.0,-0.8),
+
             ),
             Container(
               color: Color(0xFF566575),
@@ -68,6 +81,15 @@ class _HomePageState extends State<HomePage> {
                         todayColor: Color(0xFF566575),
                         selectedColor: Colors.orange,
                       ),
+                      headerStyle: HeaderStyle(
+                        centerHeaderTitle: true,
+                        formatButtonDecoration: BoxDecoration(
+                          color: Color(0xff797983),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        formatButtonTextStyle: TextStyle(color: Colors.white),
+                        formatButtonShowsNext: false,
+                      ),
                       calendarController: _control,
                     )
                   ],
@@ -78,3 +100,4 @@ class _HomePageState extends State<HomePage> {
         ))));
   }
 }
+
